0ed72e954e73029847a43dfe912b9664fb5ec806
Michael Friedrich <m.friedrich3@yahoo.com>
2023-01-10 10:50:25 +0100

    add image with loader

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -12,6 +12,10 @@ module.exports = {
         test: /\.scss$/,
         use: ["style-loader", "css-loader", "sass-loader"],
       },
+      {
+        test: /\.svg$/,
+        type: "asset/resource",
+      },
     ],
   },
   plugins: [
--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,2 @@
 import "./styles/app.scss";
+import img_world from "./images/world.svg";
