43d4dc4687299b7180d6409a8ce0d21a9e01071a
Michael Friedrich <m.friedrich3@yahoo.com>
2023-01-20 17:47:58 +0100

    add get and delete

--- src/client/api/trips.js
+++ src/client/api/trips.js
@@ -33,3 +33,43 @@ export async function uploadTrip(trip) {
     console.log("error", error);
   }
 }
+
+/**
+ * get all trips from server
+ * @returns {Array} server trip records
+ */
+export async function getTrips() {
+  const response = await fetch(url);
+  console.log("getTrips");
+  return response.json();
+}
+
+/**
+ * deletes a trip record with the given id from the server
+ * @param {Trip} trip
+ * @returns {object} deleted trip record
+ */
+export async function deleteTrip(id) {
+  try {
+    const apiResponse = await fetch(url + `/${id}`, {
+      method: "DELETE",
+    });
+
+    if (apiResponse?.ok) {
+      const tripResponse = await apiResponse.json();
+      if (tripResponse) {
+        console.log("deleted");
+        return tripResponse;
+      } else {
+        throw new Error(`Data not in json format`);
+      }
+    } else
+      throw new Error(
+        `HTTP Code: ${
+          apiResponse?.status
+        } \n Error-Message: ${await apiResponse.text()}`
+      );
+  } catch (error) {
+    console.log("error", error);
+  }
+}
